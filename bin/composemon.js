3cc4a41b0c4817aeb150d158f5641e0dc5c639ab
Matt Lubner <matt@mattlubner.com>
2016-02-20 17:59:35 -0800

    refactor: pass nodemon a config, not string

--- bin/composemon.js
+++ bin/composemon.js
@@ -10,27 +10,46 @@ var compose = require( '../lib/compose' );
 var nodemon = require( 'nodemon' );
 var path = require( 'path' );
 
-var nodemonCmd = [];
-var nodemonScript = path.resolve( path.join( __dirname, 'compose.js' ) );
-
-if ( argv.ext ) {
-  nodemonCmd.push( '--ext' );
-  nodemonCmd.push( argv.ext );
-}
-
-if ( argv.watch ) {
-  nodemonCmd.push( '--watch' );
-  nodemonCmd.push( argv.watch );
-}
-
-nodemonCmd.push( nodemonScript );
-nodemonCmd = nodemonCmd.concat( process.argv.slice( 2 ) ).join( ' ' );
-nodemon( nodemonCmd );
-
 function kill( code ) {
   process.stdout.write( '\n' );
   process.exit( code || 0 );
 }
+
+// nodemon options object
+var options = {
+  args: [],
+  script: path.resolve( path.join( __dirname, '..', 'bin', 'compose.js' ) )
+};
+
+// add nodemon args
+if ( argv.ext ) {
+  options.ext = argv.ext;
+}
+if ( argv.verbose ) {
+  options.verbose = argv.verbose;
+}
+if ( argv.watch ) {
+  options.watch = argv.watch;
+}
+
+// add docker-compose args (for compose script)
+if ( argv.file ) {
+  options.args.push( '--file' );
+  options.args.push( argv.file );
+}
+if ( argv.projectName ) {
+  options.args.push( '--project-name' );
+  options.args.push( argv.projectName );
+}
+
+// // add -- args (for docker-compose pass-thru)
+// if ( argv._.length ) {
+//   options.args.push( '--' );
+//   options.args = options.args.concat( argv._ );
+// }
+
+nodemon( options );
+console.log( 'composemon:  nodemon.config:', nodemon.config );
 
 nodemon.on( 'crash', kill );
 process.on( 'SIGINT', function () {
